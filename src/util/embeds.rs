abd2f19d65cec4694394ca3c95aa9ef0889d73eb
defnot001 <marc.miller994@gmail.com>
2024-04-20 01:24:41 +0200

    add embed colors

--- src/util/embeds.rs
+++ src/util/embeds.rs
@@ -13,3 +13,30 @@ pub fn default_embed(user: &User) -> CreateEmbed {
         .footer(footer)
         .timestamp(chrono::Utc::now())
 }
+
+#[derive(Default, poise::ChoiceParameter)]
+pub enum EmbedColor {
+    #[default]
+    Biome = 0x63A3FA,
+    Black = 0x000000,
+    Gray = 0xBEBEBE,
+    White = 0xFFFFFF,
+    Blue = 0x0000FF,
+    Cyan = 0x00FFFF,
+    Green = 0x00FF00,
+    Orange = 0xFFA500,
+    Coral = 0xFF7F50,
+    Red = 0xFF0000,
+    DeepPink = 0xFF1493,
+    Purple = 0xA020F0,
+    Magenta = 0xFF00FF,
+    Yellow = 0xFFFF00,
+    Gold = 0xFFD700,
+    None = 0x2F3136,
+}
+
+impl From<EmbedColor> for serenity::Colour {
+    fn from(colour: EmbedColor) -> Self {
+        serenity::Colour(colour as u32)
+    }
+}
